554ef1b3208273fa3fbf1892657c036213dbab6b
Hamsig <143668593+Hamsig@users.noreply.github.com>
2024-07-08 00:19:23 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 Add-Type -AssemblyName System.Windows.Forms
 
-$Urls = "https://aka.ms/vs/17/release/vc_redist.x64.exe","https://aka.ms/vs/17/release/vc_redist.x86.exe","https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe","https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.tr.exe"
+$Urls = "https://aka.ms/vs/17/release/vc_redist.x64.exe","https://aka.ms/vs/17/release/vc_redist.x86.exe","https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe","https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.tr.exe","https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x64","https://www.roblox.com/download/client?os=win","https://github.com/ppy/osu/releases/latest/download/install.exe","https://cdn-fastly.obsproject.com/downloads/OBS-Studio-30.1.2-Full-Installer-x64.exe","https://download-new.utorrent.com/endpoint/btweb/os/windows/track/stable"
 
 Write-Output "Lütfen indirme dizini seçin"
 
@@ -25,6 +25,21 @@ $Count = 0
 
 foreach ($url in $Urls) {
     $FileName = [System.IO.Path]::GetFileName($url)
+      if ($Count -eq 2) {
+          $FileName = "DirectX.exe"
+     } elseif ($Count -eq 3) {
+          $FileName = "LeagueOfLegends.exe"
+     } elseif ($Count -eq 4) {
+          $FileName = "Discord.exe"
+     } elseif ($Count -eq 5) {
+         $FileName = "Roblox.exe"
+     } elseif ($Count -eq 6) {
+         $FileName = "OsuLazer.exe"
+     } elseif ($Count -eq 7) {
+         $FileName = "Obs.exe"
+     } elseif ($Count -eq 8) {
+         $FileName = "Bittorent.exe"
+     }
 
     $input =  Read-Host "$FileName i indirmek istermisiniz? Yes yada boş bırakın"
     $Count++
@@ -41,7 +56,7 @@ foreach ($url in $Urls) {
           Start-Process -FilePath $DestinationPath -ArgumentList "/install", "/passive", "/norestart" -Wait
         } elseif ($Count -eq 3) {
           Start-Process -FilePath $DestinationPath -Wait
-        } elseif ($Count -eq 4) {
+        } else{
           Start-Process -FilePath $DestinationPath
         }
 
